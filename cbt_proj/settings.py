118846d069da2cb5ffbe654b2b707d195fa50f88
clemcy9 <clemcy9@gmail.com>
2023-06-07 01:34:26 +0100

    added whitenoise for static files rendering

--- cbt_proj/settings.py
+++ cbt_proj/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!*k1dl(a+g!v82kag08nh_5vz(ale2k)y(ew8)w-v-dm%&did2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ '*','https://cbt.up.railway.app','192.168.0.102','127.0.0.1','192.168.1.107']
 
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'cbt_proj.urls'
@@ -159,3 +160,6 @@ CSRF_TRUSTED_ORIGINS = [
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:8000',
 ]
+
+# for whitenoise static file rendering
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
